cb00b7be84a36569a38ec828b0bca4538ebb3f19
andrewor14 <andrewor14@gmail.com>
2025-07-31 06:35:00 -0700

    Deprecate old QAT APIs
    **Summary:** Deprecates QAT APIs that should no longer be used.
    Print helpful deprecation warning to help users migrate.
    
    **Test Plan:**
    ```
    python test/quantization/test_qat.py -k test_qat_api_deprecation
    ```
    
    Also manual testing:
    ```
    'IntXQuantizationAwareTrainingConfig' is deprecated and will be removed in a future release. Please use the following API instead:
    
        base_config = Int8DynamicActivationInt4WeightConfig(group_size=32)
        quantize_(model, QATConfig(base_config, step="prepare"))
        # train (not shown)
        quantize_(model, QATConfig(base_config, step="convert"))
    
    Alternatively, if you prefer to pass in fake quantization configs:
    
        activation_config = IntxFakeQuantizeConfig(torch.int8, "per_token", is_symmetric=False)
        weight_config = IntxFakeQuantizeConfig(torch.int4, group_size=32)
        qat_config = QATConfig(
            activation_config=activation_config,
            weight_config=weight_config,
            step="prepare",
        )
        quantize_(model, qat_config)
    
    Please see https://github.com/pytorch/ao/issues/2630 for more details.
    
    IntXQuantizationAwareTrainingConfig(activation_config=None, weight_config=None)
    ```
    
    ghstack-source-id: 58738f13692c6317251163d7c8ad59b115d25ddf
    Pull Request resolved: https://github.com/pytorch/ao/pull/2641

--- torchao/quantization/qat/fake_quantize_config.py
+++ torchao/quantization/qat/fake_quantize_config.py
@@ -25,6 +25,8 @@ from torchao.quantization.quant_primitives import (
     ZeroPointDomain,
 )
 
+from .utils import _log_deprecation_warning
+
 
 @dataclass
 class FakeQuantizeConfigBase(abc.ABC):
@@ -134,6 +136,14 @@ class IntxFakeQuantizeConfig(FakeQuantizeConfigBase):
         # Dynamic is not compatible with range learning
         if is_dynamic and range_learning:
             raise ValueError("`is_dynamic` is not compatible with `range_learning`")
+
+        self.__post_init__()
+
+    def __post_init__(self):
+        """
+        For deprecation only, can remove after https://github.com/pytorch/ao/issues/2630.
+        """
+        pass
 
     def _get_granularity(
         self,
@@ -261,7 +271,13 @@ class IntxFakeQuantizeConfig(FakeQuantizeConfigBase):
 
 
 # For BC
-FakeQuantizeConfig = IntxFakeQuantizeConfig
+class FakeQuantizeConfig(IntxFakeQuantizeConfig):
+    """
+    (Deprecated) Please use :class:`~torchao.quantization.qat.IntxFakeQuantizeConfig` instead.
+    """
+
+    def __post_init__(self):
+        _log_deprecation_warning(self)
 
 
 def _infer_fake_quantize_configs(
